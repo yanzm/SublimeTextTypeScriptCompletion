9bad6949ed48924ea429d1f05c0016f7c08c54f2
Yuki Anzai <yanzm@uphyca.com>
2012-11-29 18:33:01 +0900

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ SublimeTextTypeScriptCompletion
 ===============================
 
 Sublime Text 2 Plugin for TypeScript completion
+
+temporaly instaction
+
+    > sudo mkdir /opt/local/lib/node_modules/tsc_completion
+    > sudo mkdir /opt/local/lib/node_modules/tsc_completion/bin
+    > sudo cp tsc_completion/tsc_completion.js /opt/local/lib/node_modules/tsc_completion/bin/
+    > sudo cp tsc_completion/tsc_completion /opt/local/lib/node_modules/tsc_completion/bin/
+
+    > cp SublimeTextTypeScriptCompletionPlugin [Sublime Text Packages Directory]
+
---
+++ SublimeTextTypeScriptCompletionPlugin/typescriptcompletion.py
@@ -0,0 +1,63 @@
+import sublime, sublime_plugin, os, json, subprocess
+
+# https://gist.github.com/1027906
+def check_output(*popenargs, **kwargs):
+    r"""Run command with arguments and return its output as a byte string.
+
+    Backported from Python 2.7 as it's implemented as pure python on stdlib.
+
+    >>> check_output(['/usr/bin/python', '--version'])
+    Python 2.6.2
+	"""
+
+    os.environ['PATH'] = '/usr/local/bin:/opt/local/bin'
+    process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+    output, unused_err = process.communicate()
+    retcode = process.poll()
+    if retcode:
+        cmd = kwargs.get("args")
+        if cmd is None:
+            cmd = popenargs[0]
+        error = subprocess.CalledProcessError(retcode, cmd)
+        error.output = output
+        raise error
+    return output
+
+def system(command):
+	startupinfo = None
+	return check_output(command, startupinfo=startupinfo)
+
+def get_completions(view, settings):
+	comp = settings.get('typescript_completion_command')
+	if comp is None:
+		comp = 'tsc-completion'
+
+
+	point = view.sel()[0].a
+	row, col = view.rowcol(point)
+	point = point + row
+
+	command = [
+		'tsc-completion', view.file_name(), str(point), '1'
+	]
+
+	try:
+		ret = system(command)
+		return json.loads(ret)
+	except subprocess.CalledProcessError:
+		return []
+
+
+class TypeScriptCompletionListener(sublime_plugin.EventListener):
+	def __init__(self):
+		self.settings = sublime.load_settings("TypeScriptCompletion.sublime-settings");
+
+	def on_query_completions(self, view, prefix, locations):
+		if not view.file_name().endswith('.ts'):
+			return []
+
+		words = get_completions(view, self.settings)
+		entries = words["entries"]
+		words = [(w["name"] + " " + w["type"], w["name"]) for w in entries]
+
+		return words
